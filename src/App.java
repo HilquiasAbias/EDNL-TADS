524bec00eacee770d775bdb056c328732a9580e2
HilquiasAbias <hilquias.dev@gmail.com>
2024-03-13 14:12:35 -0300

    i hope this is enough

--- src/App.java
+++ src/App.java
@@ -11,30 +11,9 @@ public class App {
         tree.insert(8, "oito");
         tree.insert(22, "vinte e dois");
 
-        // System.out.println(tree.search(10));
-        // System.out.println(tree.search(5));
-        // System.out.println(tree.search(15));
-        // System.out.println(tree.search(2));
-        // System.out.println(tree.search(8));
-        // System.out.println(tree.search(22));
-
         tree.insert(25, "vinte e cinco");
-        //System.out.println(tree.search(25));
-
         tree.delete(5);
-        tree.delete(8);
-        //System.out.println(tree.search(8));
-        //System.out.println(tree.search(5));
 
-        System.out.println(tree.search(10));
-        //System.out.println(tree.search(8));
-        System.out.println(tree.search(22));
-        System.out.println(tree.search(2));
-        System.out.println(tree.search(15));
-        System.out.println(tree.search(25));
-
-        System.out.println("--------------------------------------------");
-        System.out.println(tree.search(5));
-
+        tree.print();   
     }
 }
--- src/avl/AVLTree.java
+++ src/avl/AVLTree.java
@@ -1,5 +1,8 @@
 package avl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AVLTree {
   private final String LEFT = "left";
   private final String RIGHT = "right";
@@ -26,14 +29,6 @@ public class AVLTree {
 
   public int size() {
     return size;
-  }
-
-  public Node left(Node node) {
-    return node.getLeft();
-  }
-
-  public Node right(Node node) {
-    return node.getRight();
   }
 
   public Node above(Node node) {
@@ -175,21 +170,17 @@ public class AVLTree {
   }
 
   private void updateBalanceFactorAfterDeletion(Node node, String side) {
-    if (isRoot(node)) {
-      return;
-    }
-
     if (side.equals(LEFT)) {
       node.decreaseBalanceFactor();
     } else if (side.equals(RIGHT)) {
       node.increaseBalanceFactor();
     }
 
-    if (this.isUnbalanced(node.getAbove())) {
+    if (node.getBalanceFactor() != 0 || isRoot(node)) {
+      return;
+    } else if (this.isUnbalanced(node.getAbove())) {
       this.rotate(node.getAbove());
 
-      return;
-    } else if (node.getBalanceFactor() != 0 || isRoot(node)) {
       return;
     } else {
       if (node.isLeft(node.getAbove())) {
@@ -314,21 +305,18 @@ public class AVLTree {
 
     if (!hasRight(node)) {
       String side = null;
-      if (above.isLeft(node)) {
+      if (above.getLeft() == node) {
+        System.out.println("left");
         above.setLeft(node.getLeft());
         side = LEFT;
       } else {
+        System.out.println("right");
         above.setRight(node.getLeft());
         side = RIGHT;
       }
       node.getLeft().setAbove(above);
-
-      if (node.getLeft().isLeaf()) {
-        updateBalanceFactorAfterDeletion(above, side);
-      } else {
-        updateBalanceFactorAfterDeletion(node.getLeft(), side);
-      }
-      //updateBalanceFactorAfterDeletion(node.getLeft(), side);
+      updateBalanceFactorAfterDeletion(above, side);
+      node.setAbove(null);
       return;
     } else {
       Node sucessor = successor(node);
@@ -376,6 +364,110 @@ public class AVLTree {
       throw new NodeNotFoundException(Integer.toString(key));
     } else {
       return node;
+    }
+  }
+
+  public void print() {
+    List<List<String>> lines = new ArrayList<List<String>>();
+
+    List<Node> level = new ArrayList<Node>();
+    List<Node> next = new ArrayList<Node>();
+
+    level.add(root);
+    int nn = 1;
+
+    int widest = 0;
+
+    while (nn != 0) {
+      List<String> line = new ArrayList<String>();
+
+      nn = 0;
+
+      for (Node n : level) {
+        if (n == null) {
+          line.add(null);
+
+          next.add(null);
+          next.add(null);
+        } else {
+          String aa = String.valueOf(n.getKey()) + "(" + n.getBalanceFactor() + ")";
+          line.add(aa);
+          if (aa.length() > widest) widest = aa.length();
+
+          next.add(n.getLeft());
+          next.add(n.getRight());
+
+          if (n.getLeft() != null) nn++;
+          if (n.getRight() != null) nn++;
+        }
+      }
+
+      if (widest % 2 == 1) widest++;
+
+      lines.add(line);
+
+      List<Node> tmp = level;
+      level = next;
+      next = tmp;
+      next.clear();
+    }
+
+    int perpiece = lines.get(lines.size() - 1).size() * (widest + 4);
+    for (int i = 0; i < lines.size(); i++) {
+      List<String> line = lines.get(i);
+      int hpw = (int) Math.floor(perpiece / 2f) - 1;
+
+      if (i > 0) {
+        for (int j = 0; j < line.size(); j++) {
+
+          // split node
+          char c = ' ';
+          if (j % 2 == 1) {
+            if (line.get(j - 1) != null) {
+              c = (line.get(j) != null) ? '┴' : '┘';
+            } else {
+              if (j < line.size() && line.get(j) != null) c = '└';
+            }
+          }
+          System.out.print(c);
+
+          // lines and spaces
+          if (line.get(j) == null) {
+            for (int k = 0; k < perpiece - 1; k++) {
+              System.out.print(" ");
+            }
+          } else {
+            for (int k = 0; k < hpw; k++) {
+                System.out.print(j % 2 == 0 ? " " : "─");
+            }
+            System.out.print(j % 2 == 0 ? "┌" : "┐");
+            for (int k = 0; k < hpw; k++) {
+              System.out.print(j % 2 == 0 ? "─" : " ");
+            }
+          }
+        }
+        System.out.println();
+      }
+
+      // print line of numbers
+      for (int j = 0; j < line.size(); j++) {
+        String f = line.get(j);
+        if (f == null) f = "";
+        int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
+        int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
+
+        // a number
+        for (int k = 0; k < gap1; k++) {
+          System.out.print(" ");
+        }
+        System.out.print(f);
+        for (int k = 0; k < gap2; k++) {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+
+      perpiece /= 2;
     }
   }
 }
