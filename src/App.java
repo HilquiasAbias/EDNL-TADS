a43948720d073c3bc79366f9639ac6a0b06f25a3
HilquiasAbias <hilquias.dev@gmail.com>
2024-03-15 15:11:48 -0300

    working on insertion

--- src/App.java
+++ src/App.java
@@ -1,8 +1,23 @@
 import tree.avl.*;
+import tree.rb.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        AVLTree tree = new AVLTree();
+        // AVLTree tree = new AVLTree();
+
+        // tree.insert(10, "dez");
+        // tree.insert(5, "cinco");
+        // tree.insert(15, "quinze");
+        // tree.insert(2, "dois");
+        // tree.insert(8, "oito");
+        // tree.insert(22, "vinte e dois");
+
+        // tree.insert(25, "vinte e cinco");
+        // tree.delete(5);
+
+        // tree.print(); 
+        
+        RBTree tree = new RBTree();
 
         tree.insert(10, "dez");
         tree.insert(5, "cinco");
@@ -10,10 +25,8 @@ public class App {
         tree.insert(2, "dois");
         tree.insert(8, "oito");
         tree.insert(22, "vinte e dois");
+        tree.insert(9, "nove");
 
-        tree.insert(25, "vinte e cinco");
-        tree.delete(5);
-
-        tree.print();   
+        tree.print();
     }
 }
--- src/tree/rb/RBTree.java
+++ src/tree/rb/RBTree.java
@@ -1,5 +1,8 @@
 package tree.rb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RBTree {
   private final String LEFT = "left";
   private final String RIGHT = "right";
@@ -154,21 +157,29 @@ public class RBTree {
 
   private void checkIntegrityOfRulesAfterInsertion(Node insertedNode) {
     Node above = insertedNode.getAbove();
+
     if (above.getColor() == BLACK) {
       return;
     }
 
-    Node nextToAbove = getNextToAbove(insertedNode);
+    recolorAboveAndNextToIt(above);
+  }
+
+  private void recolorAboveAndNextToIt(Node node) {
+    Node above = node.getAbove();
+    Node nextToAbove = getNextToAbove(node);
+    
     if (nextToAbove.getColor() == RED) {
-      above.setColor(BLACK);
+      node.setColor(BLACK);
       nextToAbove.setColor(BLACK);
-      if (isRoot(above.getAbove())) {
+
+      if (isRoot(above)) {
         return;
       } 
       
-      above.getAbove().setColor(RED);
+      above.setColor(RED);
 
-      // verificar outras casos
+      recolorAboveAndNextToIt(above);
     }
   }
 
@@ -178,6 +189,110 @@ public class RBTree {
       return above.getRight();
     } else {
       return above.getLeft();
+    }
+  }
+
+  public void print() {
+    List<List<String>> lines = new ArrayList<List<String>>();
+    
+    List<Node> level = new ArrayList<Node>();
+    List<Node> next = new ArrayList<Node>();
+
+    level.add(root);
+    int nn = 1;
+
+    int widest = 0;
+
+    while (nn != 0) {
+      List<String> line = new ArrayList<String>();
+
+      nn = 0;
+
+      for (Node n : level) {
+        if (n == null) {
+          line.add(null);
+
+          next.add(null);
+          next.add(null);
+        } else {
+          String aa = String.valueOf(n.getKey()) + "(BF=" + n.getBalanceFactor() + ", C=" + (n.getColor() == 0 ? "R" : "B" ) + ")";
+          line.add(aa);
+          if (aa.length() > widest) widest = aa.length();
+
+          next.add(n.getLeft());
+          next.add(n.getRight());
+
+          if (n.getLeft() != null) nn++;
+          if (n.getRight() != null) nn++;
+        }
+      }
+
+      if (widest % 2 == 1) widest++;
+
+      lines.add(line);
+
+      List<Node> tmp = level;
+      level = next;
+      next = tmp;
+      next.clear();
+    }
+
+    int perpiece = lines.get(lines.size() - 1).size() * (widest + 4);
+    for (int i = 0; i < lines.size(); i++) {
+      List<String> line = lines.get(i);
+      int hpw = (int) Math.floor(perpiece / 2f) - 1;
+
+      if (i > 0) {
+        for (int j = 0; j < line.size(); j++) {
+
+          // split node
+          char c = ' ';
+          if (j % 2 == 1) {
+            if (line.get(j - 1) != null) {
+              c = (line.get(j) != null) ? '┴' : '┘';
+            } else {
+              if (j < line.size() && line.get(j) != null) c = '└';
+            }
+          }
+          System.out.print(c);
+
+          // lines and spaces
+          if (line.get(j) == null) {
+            for (int k = 0; k < perpiece - 1; k++) {
+              System.out.print(" ");
+            }
+          } else {
+            for (int k = 0; k < hpw; k++) {
+                System.out.print(j % 2 == 0 ? " " : "─");
+            }
+            System.out.print(j % 2 == 0 ? "┌" : "┐");
+            for (int k = 0; k < hpw; k++) {
+              System.out.print(j % 2 == 0 ? "─" : " ");
+            }
+          }
+        }
+        System.out.println();
+      }
+
+      // print line of numbers
+      for (int j = 0; j < line.size(); j++) {
+        String f = line.get(j);
+        if (f == null) f = "";
+        int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
+        int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
+
+        // a number
+        for (int k = 0; k < gap1; k++) {
+          System.out.print(" ");
+        }
+        System.out.print(f);
+        for (int k = 0; k < gap2; k++) {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+
+      perpiece /= 2;
     }
   }
 }
